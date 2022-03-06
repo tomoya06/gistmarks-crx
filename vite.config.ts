e05e95605186d8ab338090f859396dba76f2aa06
tomoya06 <peng4766@gmail.com>
2022-03-06 16:40:26 +0800

    config: og approach

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import copy from "rollup-plugin-copy";
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [{ src: "./manifest.json", dest: "dist/" }],
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
